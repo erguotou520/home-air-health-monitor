3e05d3957b3225829115671b2c2fe29cd9ee3604
erguotou <erguotou525@gmail.com>
2018-12-12 11:09:08 +0800

    添加定时任务

--- pi/app.py
+++ pi/app.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*
 import signal
+import atexit
 from flask import Flask, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
 import RPi.GPIO as GPIO
 from config import config
 from data import read_all_data_onetime
+from data import read_all_data
 
-def cleanup():
+def shutdown():
+  sched.shutdown(wait=False)
   GPIO.cleanup()
 
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
+signal.signal(signal.SIGTERM, shutdown)
+signal.signal(signal.SIGINT, shutdown)
+atexit.register(shutdown)
+
+# scheduler
+sched = BackgroundScheduler()
+sched.add_job(read_all_data, 'cron', minute=0)
+sched.add_job(read_all_data, 'cron', minute=15)
+sched.add_job(read_all_data, 'cron', minute=30)
+sched.add_job(read_all_data, 'cron', minute=45)
 
 app = Flask(__name__)
 
@@ -23,6 +35,6 @@ def get_data():
   return jsonify(data)
 
 if __name__ == '__main__':
-  # read_all_data_onetime()
+  sched.start()
   app.debug = True
   app.run(config['server']['host'], config['server']['port'])
